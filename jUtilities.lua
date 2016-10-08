59f2f56afeede442efc563098723820e580d51fd
jwldnr <alexander@woldner.io>
2016-10-08 12:50:38 +0200

    misc updates

--- jUtilities.lua
+++ jUtilities.lua
@@ -38,6 +38,25 @@ function Addon:OnEvent(event, ...)
   end
 end
 
+-- hooks
+do
+  local function Minimap_OnMouseWheel(frame, delta)
+    Addon:MinimapOnMouseWheel(frame, delta);
+  end
+
+  function Addon:HookActionEvents()
+    Minimap:SetScript('OnMouseWheel', Minimap_OnMouseWheel);
+  end
+end
+
+function Addon:MinimapOnMouseWheel(frame, delta)
+  if (delta > 0) then
+    Minimap_ZoomIn();
+  else
+    Minimap_ZoomOut();
+  end
+end
+
 function Addon:OnUpdate(elapsed)
   lastUpdateTime = lastUpdateTime + elapsed;
 
@@ -57,6 +76,8 @@ function Addon:OnUpdate(elapsed)
 end
 
 function Addon:PLAYER_LOGIN()
+  self:HookActionEvents();
+
   -- set up alias reload slash command
   SLASH_RL1 = '/rl';
   function SlashCmdList.RL(msg, editbox)
@@ -64,13 +85,13 @@ function Addon:PLAYER_LOGIN()
   end
 
   -- set status text
-  SetCVar('statusText', '0');
+  SetCVar('statusText', 0);
 
   -- ensure key press on key down
-  SetCVar('ActionButtonUseKeyDown', '1');
+  SetCVar('ActionButtonUseKeyDown', 1);
 
   -- enable taint logging
-  SetCVar('taintLog', '1'); -- 2
+  SetCVar('taintLog', 1); -- 2
 
   -- disable secure ability toggle
   SetCVar('secureAbilityToggle', 0);
@@ -82,6 +103,20 @@ function Addon:PLAYER_LOGIN()
 
   -- enable LUA errors
   SetCVar('scriptErrors', 1);
+
+  -- disable click-to-move
+  SetCVar('autoInteract', 0);
+
+  -- enable mouse wheel over minimap
+  Minimap:EnableMouseWheel(true);
+
+  -- hide the minimap zoom buttons
+  MinimapZoomIn:Hide();
+  MinimapZoomOut:Hide();
+
+  -- move the minimap tracking icon
+  MiniMapTracking:ClearAllPoints();
+  MiniMapTracking:SetPoint('TOPRIGHT', -26, 7);
 end
 
 -- call
