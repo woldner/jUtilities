c42874224f3f55fa1bf25825d741056bdb7a18f1
Jean Alexander Woldner <alexander@woldner.io>
2017-03-04 09:50:00 +0100

    remove unnecessary semicolons

--- jUtilities.lua
+++ jUtilities.lua
@@ -1,123 +1,123 @@
 -- locals and speed
-local AddonName, Addon = ...;
+local AddonName, Addon = ...
 
-local select = select;
+local select = select
 
-local CreateFrame = CreateFrame;
-local GetCVar = GetCVar;
-local GetNetStats = GetNetStats;
+local CreateFrame = CreateFrame
+local GetCVar = GetCVar
+local GetNetStats = GetNetStats
 
-local currentTolerance = GetCVar('maxSpellStartRecoveryOffset');
-local lastUpdateTime = 0;
+local currentTolerance = GetCVar('maxSpellStartRecoveryOffset')
+local lastUpdateTime = 0
 
 -- main
 function Addon:Load()
   do
-    local eventHandler = CreateFrame('Frame', nil);
+    local eventHandler = CreateFrame('Frame', nil)
 
     -- set OnEvent handler
     eventHandler:SetScript('OnEvent', function(handler, ...)
-        self:OnEvent(...);
+        self:OnEvent(...)
       end)
 
     -- set OnUpdate handler
     eventHandler:SetScript('OnUpdate', function(handler, ...)
-        self:OnUpdate(...);
+        self:OnUpdate(...)
       end)
 
-    eventHandler:RegisterEvent('PLAYER_LOGIN');
+    eventHandler:RegisterEvent('PLAYER_LOGIN')
   end
 end
 
 -- frame events
 function Addon:OnEvent(event, ...)
-  local action = self[event];
+  local action = self[event]
 
   if (action) then
-    action(self, event, ...);
+    action(self, event, ...)
   end
 end
 
 -- hooks
 do
   local function Minimap_OnMouseWheel(frame, delta)
-    Addon:MinimapOnMouseWheel(frame, delta);
+    Addon:MinimapOnMouseWheel(frame, delta)
   end
 
   function Addon:HookActionEvents()
-    Minimap:SetScript('OnMouseWheel', Minimap_OnMouseWheel);
+    Minimap:SetScript('OnMouseWheel', Minimap_OnMouseWheel)
   end
 end
 
 function Addon:MinimapOnMouseWheel(frame, delta)
   if (delta > 0) then
-    Minimap_ZoomIn();
+    Minimap_ZoomIn()
   else
-    Minimap_ZoomOut();
+    Minimap_ZoomOut()
   end
 end
 
 function Addon:OnUpdate(elapsed)
-  lastUpdateTime = lastUpdateTime + elapsed;
+  lastUpdateTime = lastUpdateTime + elapsed
 
   -- limit update to once per second
   if (lastUpdateTime < 1.0) then return end
-  lastUpdateTime = 0;
+  lastUpdateTime = 0
 
   -- get world latency
-  local newTolerance = select(4, GetNetStats());
+  local newTolerance = select(4, GetNetStats())
 
   -- ignore empty value and prevent update spam
   if (newTolerance == 0 or newTolerance == currentTolerance) then return end
-  currentTolerance = newTolerance;
+  currentTolerance = newTolerance
 
   -- set lag tolerance
-  SetCVar('maxSpellStartRecoveryOffset', newTolerance);
+  SetCVar('maxSpellStartRecoveryOffset', newTolerance)
 end
 
 function Addon:PLAYER_LOGIN()
-  self:HookActionEvents();
+  self:HookActionEvents()
 
   -- set up alias reload slash command
-  SLASH_RL1 = '/rl';
+  SLASH_RL1 = '/rl'
   function SlashCmdList.RL(msg, editbox)
-    ReloadUI();
+    ReloadUI()
   end
 
   -- set status text
-  SetCVar('statusText', 0);
+  SetCVar('statusText', 0)
 
   -- ensure key press on key down
-  SetCVar('ActionButtonUseKeyDown', 1);
+  SetCVar('ActionButtonUseKeyDown', 1)
 
   -- enable taint logging
-  SetCVar('taintLog', 1); -- 2
+  SetCVar('taintLog', 1) -- 2
 
   -- disable secure ability toggle
-  SetCVar('secureAbilityToggle', 0);
+  SetCVar('secureAbilityToggle', 0)
 
   -- enable Custom Lag Tolerance (0-400 ms)
-  SetCVar('reducedLagTolerance', 1);
+  SetCVar('reducedLagTolerance', 1)
   -- with a initial value of 50
-  SetCVar('maxSpellStartRecoveryOffset', 50);
+  SetCVar('maxSpellStartRecoveryOffset', 50)
 
   -- enable LUA errors
-  SetCVar('scriptErrors', 1);
+  SetCVar('scriptErrors', 1)
 
   -- disable click-to-move
-  SetCVar('autoInteract', 0);
+  SetCVar('autoInteract', 0)
 
   -- enable mouse wheel over minimap
-  Minimap:EnableMouseWheel(true);
+  Minimap:EnableMouseWheel(true)
 
   -- hide the minimap zoom buttons
-  MinimapZoomIn:Hide();
-  MinimapZoomOut:Hide();
+  MinimapZoomIn:Hide()
+  MinimapZoomOut:Hide()
 
   -- move the minimap tracking icon
-  MiniMapTracking:ClearAllPoints();
-  MiniMapTracking:SetPoint('TOPRIGHT', -26, 7);
+  MiniMapTracking:ClearAllPoints()
+  MiniMapTracking:SetPoint('TOPRIGHT', -26, 7)
 end
 
 -- call
-Addon:Load();
+Addon:Load()
